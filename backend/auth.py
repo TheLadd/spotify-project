12fef44ecd331c0c2056999a02f359434188f7ea
TheLadd <owenribera@gmail.com>
2022-12-16 12:42:36 -0800

    Got rudimentary version of song-fetching algorithm working

--- backend/auth.py
+++ backend/auth.py
@@ -1,13 +1,16 @@
 import spotipy
-# sp.user_create_playlist()
-# sp.search(q=myQuery, type='track')
-#   i believe sp api queries take the form of "artist:<artist> track:<track> genre:<genre> etc.." (of type string)
-
-CLIENT_ID = "a747d052efc94b4a9b8db1bfd0c31fa1"
-CLIENT_SECRET= "de5a3b6b6cd94d4f89444e187268a1bc"
+import os
+#CLIENT_ID = "a747d052efc94b4a9b8db1bfd0c31fa1"
+CLIENT_ID = "de196a5035cb451499b1a2b29c58aac9"
+CLIENT_SECRET= "50a107e67115470f87f801954665b81b"
 REDIRECT_URI= "http://localhost:5000"
 SCOPE= "playlist-modify-public" # this will definitely need to change
 UN= "ribbsauce"
+
+# Temporarilly here
+os.environ["SPOTIPY_CLIENT_ID"]=CLIENT_ID
+os.environ["SPOTIPY_CLIENT_SECRET"]=CLIENT_SECRET
+os.environ["SPOTIPY_REDIRECT_URI"]=REDIRECT_URI
 
 sp_oauth = spotipy.oauth2.SpotifyOAuth(
     client_id=CLIENT_ID,
@@ -17,4 +20,7 @@ sp_oauth = spotipy.oauth2.SpotifyOAuth(
     username=UN,
 )
 
-#sp = spotipy.Spotify(auth_manager=sp_oauth)
+# sp = spotipy.Spotify(auth_manager=sp_oauth)
+# sp.user_create_playlist()
+# sp.search(q=myQuery, type='track')
+#   i believe sp api queries take the form of "artist:<artist> track:<track> genre:<genre> etc.." (of type string)
--- backend/strToPlay.py
+++ backend/strToPlay.py
@@ -1,1 +1,76 @@
 # File for the *algorithm*
+import spotipy
+import auth
+
+sp = spotipy.Spotify(auth_manager=auth.sp_oauth)
+
+#myQuery = "surfer"
+#results = sp.search(q="track: " + myQuery, type='track')
+#items = results['tracks']['items']
+#
+#for i in range(len(items)):
+#    print(items[i]['name'], " | ", items[i]['artists'][0]['name'])
+
+def getSongs(spSession, tarWords):
+    """
+    Input:
+        spSession: spotipy session
+        tarWords: List[str] that is user input that we want to replicate with titles in a playlist
+    Return:
+        List/Tuple of Spotify Track URIs 
+    """
+    # Development Notes
+    #   Base case is not explicit, but implied in the case of 'trackList = []' upon  
+
+    if len(tarWords) == 0:
+        return []
+
+    trackList = []
+
+    # 1. Tokenize
+    #words = tarWords.lower()
+    words = [x.lower() for x in tarWords]
+
+    # 2. Look for matching titles, prioritizing short titles
+    for i in range(len(words)):
+        target = " ".join(words[:i+1])
+        results = spSession.search(q="track: "+target, type="track")['tracks']
+        print(f"target #{i}: {target}")
+
+        while(results['next'] != None):
+            items = results['items']
+            for j in range(len(items)):
+                track = items[j]
+                print(f"track name: {track['name']}")
+                if track['name'].lower() == target:
+                    nextTracks = getSongs(spSession, tarWords[i+1:])
+
+                    # 1. Check if the process can finish with the word being added
+                    if nextTracks == None:
+                        continue
+                    
+                    # 2. If so, add this track and it's subsequent results
+                    trackList.append(track)
+                    trackList += nextTracks
+                    break
+            if len(trackList) > 0:
+                break
+            results = spSession.next(results)['tracks']
+
+        if len(trackList) > 0:
+            break
+            
+    if len(trackList) == 0:
+        return None
+    return trackList
+
+myString = "Sad boy hours my love"
+myList = myString.split()
+
+
+def printSongNames(myList):
+    for x in myList:
+        print(x['name'], " | ", x['artists'][0]['name'])
+
+
+printSongNames(getSongs(sp, myList)) 
