f2e0525626f07875317f3bda4c5aef0d23077f8b
TheLadd <owenribera@gmail.com>
2022-12-17 12:28:52 -0800

    Cleaned up getSongs() and made it a bit more efficient

--- backend/strToPlay.py
+++ backend/strToPlay.py
@@ -4,73 +4,57 @@ import auth
 
 sp = spotipy.Spotify(auth_manager=auth.sp_oauth)
 
-#myQuery = "surfer"
-#results = sp.search(q="track: " + myQuery, type='track')
-#items = results['tracks']['items']
-#
-#for i in range(len(items)):
-#    print(items[i]['name'], " | ", items[i]['artists'][0]['name'])
-
 def getSongs(spSession, tarWords):
     """
     Input:
         spSession: spotipy session
         tarWords: List[str] that is user input that we want to replicate with titles in a playlist
     Return:
-        List/Tuple of Spotify Track URIs 
+        List of Spotify Track URIs 
     """
-    # Development Notes
-    #   Base case is not explicit, but implied in the case of 'trackList = []' upon  
 
+    # Base case
     if len(tarWords) == 0:
         return []
 
-    trackList = []
-
-    # 1. Tokenize
-    #words = tarWords.lower()
+    # 1. Pre-process
     words = [x.lower() for x in tarWords]
 
-    # 2. Look for matching titles, prioritizing short titles
+    # 2. Look for titles matching the first i words (prioritizing short titles)
     for i in range(len(words)):
         target = " ".join(words[:i+1])
-        results = spSession.search(q="track: "+target, type="track")['tracks']
-        print(f"target #{i}: {target}")
+        results = spSession.search(q="track: "+target, type="track")
+        results = results['tracks']
+        k = 0
 
-        while(results['next'] != None):
+        # Cycle through first 10 pages of search results
+        while results['next'] != None and k < 10:
             items = results['items']
+
+            # Look through a page of search results
             for j in range(len(items)):
                 track = items[j]
-                print(f"track name: {track['name']}")
                 if track['name'].lower() == target:
                     nextTracks = getSongs(spSession, tarWords[i+1:])
-
-                    # 1. Check if the process can finish with the word being added
-                    if nextTracks == None:
-                        continue
-                    
-                    # 2. If so, add this track and it's subsequent results
-                    trackList.append(track)
-                    trackList += nextTracks
+                    if nextTracks != None:
+                        return [track] + nextTracks
                     break
-            if len(trackList) > 0:
-                break
+
+            k += 1
             results = spSession.next(results)['tracks']
 
-        if len(trackList) > 0:
-            break
-            
-    if len(trackList) == 0:
-        return None
-    return trackList
-
-myString = "Sad boy hours my love"
-myList = myString.split()
-
+    return None 
 
 def printSongNames(myList):
+    if myList == None:
+        print("Couldn't find what you were looking for!")
+        return
     for x in myList:
         print(x['name'], " | ", x['artists'][0]['name'])
 
 
+#myString = "Sad boy hours my love"
+myString = "I'm finding it hard to carry on"
+myString = "Hey friend you suck"
+myList = myString.split()
 printSongNames(getSongs(sp, myList)) 
